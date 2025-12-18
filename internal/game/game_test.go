343a016ee06785888187697bd2066d1472de9a07
Hadi T <hadi@syniol.com>
2025-12-18 11:03:47 +0000

    Completed the tests

--- internal/game/game_test.go
+++ internal/game/game_test.go
@@ -1,6 +1,9 @@
 package game
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestNewGame(t *testing.T) {
 	sut := NewGame()
@@ -26,24 +29,46 @@ func TestTwoPlayers(t *testing.T) {
 		t.Errorf("Opponent should be %s", OpponentHuman)
 	}
 
-	// todo: could be a matrix of all choices in iteration for all possible outcomes
+	for choice, opposeChoice := range Rules {
+		sut.Players[0].Name = "John"
+		sut.Players[0].Choice = choice
+
+		for opponentChoice, reason := range opposeChoice {
+			sut.Players[1].Name = "Alice"
+			sut.Players[1].Choice = opponentChoice
+
+			result := sut.Start()
+
+			if result.Winner.Name != sut.Players[0].Name {
+				t.Errorf("Winner should be Alice but %s has won", result.Winner.Name)
+			}
+
+			if result.Reason != fmt.Sprintf("%s %s", sut.Players[0].Choice, reason) {
+				t.Errorf("Reason should include %s", reason)
+			}
+
+			if sut.Scoreboard.storage[sut.Players[0].Name] != 1 {
+				t.Errorf("Scoreboard should have a winning score record for Alice")
+			}
+
+			sut.Scoreboard.Reset()
+		}
+	}
+}
+
+func TestSinglePlayer(t *testing.T) {
+	sut := NewGame()
+
 	sut.Players[0].Name = "John"
 	sut.Players[0].Choice = Rock
 
-	sut.Players[1].Name = "Alice"
-	sut.Players[1].Choice = Paper
+	score := sut.Start()
 
-	result := sut.Start()
-
-	if result.Winner.Name != sut.Players[1].Name {
-		t.Errorf("Winner should be Alice but %s has won", sut.Start().Winner.Name)
+	if len(score.Status) == 0 {
+		t.Error("Expected a Status")
 	}
 
-	if result.Reason != "Paper Covers Rock" {
-		t.Errorf("Reason should be %s", sut.Start().Reason)
-	}
-
-	if sut.Scoreboard.storage[sut.Players[1].Name] != 1 {
-		t.Errorf("Scoreboard should have a winning score record for Alice")
+	if len(score.Reason) == 0 {
+		t.Error("Expected a reason")
 	}
 }
